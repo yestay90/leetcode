08f903547c23c14bb57f0904bce37ec5f26fb1d7
yestay90 <yestay90@gmail.com>
2023-04-18 13:41:57 +0600

    added class

--- ZigzagConversion/ZigzagConversion/main.cpp
+++ ZigzagConversion/ZigzagConversion/main.cpp
@@ -7,53 +7,59 @@
 
 #include <iostream>
 using namespace std;
+class Solution {
+    public:
+    string convert(string s, int numRows) {
+        int currentRow = 0;
+        int currentCol = 0;
 
-string convert(string s, int numRows) {
-    int currentRow = 0;
-    int currentCol = 0;
-
-    char ** A;
-    A = new char*[numRows];
-    for (int a = 0; a < numRows; a++)
-        A[a] = new char[s.length()];
-    
-    A[currentRow][currentCol] = s[0];
-    string converted;
-    int i = 1;
-    bool isRowDirection = true;
-    
-    while (i < s.length()) {
-        if (isRowDirection) {
-            if (currentRow + 1 < numRows) {
-                A[++currentRow][currentCol] = s[i];
-                i++;
+        char ** A;
+        A = new char*[numRows];
+        for (int a = 0; a < numRows; a++)
+            A[a] = new char[s.length()];
+        
+        A[currentRow][currentCol] = s[0];
+        string converted;
+        int i = 1;
+        bool isRowDirection = true;
+        
+        while (i < s.length()) {
+            if (isRowDirection) {
+                if (currentRow + 1 < numRows) {
+                    A[++currentRow][currentCol] = s[i];
+                    i++;
+                } else {
+                    isRowDirection = false;
+                }
             } else {
-                isRowDirection = false;
-            }
-        } else {
-            if (currentRow > 0 && currentCol + 1 < s.length()) {
-                A[--currentRow][++currentCol] = s[i];
-                i++;
-            } else {
-                isRowDirection = true;
+                if (currentRow > 0 && currentCol + 1 < s.length()) {
+                    A[--currentRow][++currentCol] = s[i];
+                    i++;
+                } else {
+                    isRowDirection = true;
+                }
             }
         }
-    }
-    
-    for (int r = 0; r < numRows; r++){
-        for (int c = 0; c < s.length(); c++) {
-            if (A[r][c])
-                converted += A[r][c];
+        
+        for (int r = 0; r < numRows; r++){
+            for (int c = 0; c < s.length(); c++) {
+                if (A[r][c])
+                    converted += A[r][c];
+            }
         }
+        
+        return converted;
     }
-    
-    return converted;
-}
+};
+
+
 int main(int argc, const char * argv[]) {
+    
+    Solution myObj;
   
     string s = "YEstayMuratov";
     
-    string v = convert(s, 4);
+    string v = myObj.convert(s, 4);
     
     cout<<v<<endl;
     
